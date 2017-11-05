f07519a7be3b92162fa21d61ae515625e9284164
Kyle McCain <chiefmccain85@hotmail.com>
2017-11-05 05:50:05 -0600

    Added additional set/get methods and changed the type of class for lossDate from Date to the new LocalDate class.

--- PatriotCrewManagement/src/Soldier.java
+++ PatriotCrewManagement/src/Soldier.java
@@ -1,4 +1,4 @@
-import java.util.Date;
+import java.time.LocalDate;
 
 /*
  * License: There is no applicable license
@@ -15,7 +15,7 @@ public class Soldier {
 	private String lastName;
 	private ArmyRank rank;
 	private String mos;
-	private Date lossDate;
+	private LocalDate lossDate;
 	
 	// Constructor with just last name and rank
 	public Soldier(String lastName, String rankString) {
@@ -24,7 +24,7 @@ public class Soldier {
 	}
 	
 	// Full constructor to initialize all class members
-	public Soldier(String firstName, String lastName, String rankString, String mos, Date lossDate) {
+	public Soldier(String firstName, String lastName, String rankString, String mos, LocalDate lossDate) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.rank = ArmyRank.getRank(rankString);
@@ -37,11 +37,21 @@ public class Soldier {
 		return firstName;
 	}
 	
+	// Set method for Soldier's first name
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
 	// Get method for Soldier's last name
 	public String getLastName() {
 		return lastName;
 	}
 	
+	// Set method for Soldier's last name
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	// Get method for Soldier's rank as an ArmyRank object
 	public ArmyRank getRank() {
 		return rank;
@@ -52,16 +62,45 @@ public class Soldier {
 		return rank.getRankShort();
 	}
 	
+	// Set method for Soldier's rank
+	public void setRank(ArmyRank rank) {
+		this.rank = rank;
+	}
+	
+	// set method for Soldier's rank as a String
+	public boolean setRankString(String rank) {
+		ArmyRank newRank = ArmyRank.getRank(rank);
+
+		// Returns true if the input string was a valid short-hand rank
+		if (newRank == null) { 
+			return false;
+		}
+		else {
+			this.rank = newRank;
+			return true;
+		}
+	}
+
 	// Get method for Soldier's MOS
 	public String getMOS() {
 		return mos;
 	}
 	
+	// Set method for Soldier's MOS
+	public void setMos(String mos) {
+		this.mos = mos;
+	}
+
 	// Get method for Soldier's loss date
-	public Date getLossDate() {
+	public LocalDate getLossDate() {
 		return lossDate;
 	}
 	
+	// Set method for Soldier's loss date
+	public void setLossDate(LocalDate lossDate) {
+		this.lossDate = lossDate;
+	}
+
 	// Returns whether Soldier out-ranks the Soldier passed in
 	public boolean outrank(Soldier otherSoldier) {
 		if (this.rank.compareTo(otherSoldier.rank) <= 0) {
