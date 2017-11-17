df4457663370e13de1c5a856f14612a42de17900
Kyle McCain <chiefmccain85@hotmail.com>
2017-11-17 07:23:53 +0300

    Reduced AMGCrew subclass down to just the number of crewmembers in crew and what their position titles are.

--- PatriotCrewManagement/src/AMGCrew.java
+++ PatriotCrewManagement/src/AMGCrew.java
@@ -12,18 +12,6 @@ public class AMGCrew extends ArmyCrew {
 		{"Crewmember 1", "Crewmember 2", "Crewmember 3"};
 	
 	public AMGCrew() {
-		super(new CrewMember[3]);
-	}
-	
-	// Adds a crew member to the AMG crew and returns true if successfully added
-	public boolean addCrewMember(Soldier soldier, int crewPosition) {
-		if (crewPosition <= TITLES.length) {
-			CrewMember amgCrewMember = new CrewMember(soldier, 
-					TITLES[crewPosition - 1], crewPosition);
-			
-			return insertCrewMember(amgCrewMember, crewPosition);
-		} else {
-			return false;
-		}
+		super(new CrewMember[3], TITLES);
 	}
 }
